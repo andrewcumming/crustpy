ed85067362c5bb59a2af3957f631ca4d2f241491
Andrew Cumming <andrew.cumming@gmail.com>
2014-03-22 02:21:37 -0400

    Simplified composition routine

--- crust.py
+++ crust.py
@@ -29,13 +29,10 @@ class Crust:
 		Acella=[56.0,56.0,56.0,56.0,56.0,56.0,56.0,56.0,112.0,112.0,112.0,112.0,112.0,224.0,224.0,224.0,224.0,448.0,448.0]
 		Aa=[56.0,56.0,56.0,56.0,56.0,52.0,46.0,40.0,68.0,62.0,56.0,50.0,44.0,66.0,60.0,54.0,48.0,96.0,88.0];
 		Za=[26.0,24.0,22.0,20.0,18.0,16.0,14.0,12.0,20.0,18.0,16.0,14.0,12.0,18.0,16.0,14.0,12.0,24.0,22.0];
-		Pmax = [7.235e26,9.569e27,1.152e29,4.747e29,1.361e30,1.980e30,2.253e30,2.637e30,2.771e30,3.216e30,3.825e30,4.699e30,6.043e30,7.233e30,9.238e30,1.228e31,1.602e31,1.613e31,1e33]
-		for (Acell1,A1,Z1,Pmax1) in zip(Acella,Aa,Za,Pmax):
-			if P<numpy.log10(Pmax1):
-				Acell=Acell1
-				A=A1
-				Z=Z1
-				break
+		# Maximum pressure at which that composition is present
+		Pmaxa = [7.235e26,9.569e27,1.152e29,4.747e29,1.361e30,1.980e30,2.253e30,2.637e30,2.771e30,3.216e30,3.825e30,4.699e30,6.043e30,7.233e30,9.238e30,1.228e31,1.602e31,1.613e31,1e33]
+		# Find the composition in the first entry in the table where P>Pmax.
+		(Acell,A,Z) = [(Acell,A,Z) for (Acell,A,Z,Pmax) in zip(Acella,Aa,Za,Pmaxa) if Pmax>10.0**P][0]
 		Yn = (Acell-A)/Acell
 		return (A,Z,Yn)
 
